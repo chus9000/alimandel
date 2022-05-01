fafc1bfff475cd045c9ba67fb266af1005e9aee6
Jesus Martin <jesusmartin@MacBook-Air-de-Jesus.local>
2022-05-01 18:21:20 +0200

    minor bugs

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="Como jugar" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Cómo jugar" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         Adivina la palabra en 6 intentos. Después de cada intento, el color de las letras cambiará para mostrarte como de cerca estuviste de acertar.
       </p>
@@ -46,11 +46,11 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
+        <Cell value="M" />
         <Cell value="A" />
-        <Cell value="G" />
+        <Cell value="M" />
         <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="T" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         La letra U no está en la palabra en ninguna posición.
